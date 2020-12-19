20ebc917c7f461144b58b2aba67f7a288ca5fd2c
Vinicius Bignardi <vinicius.bignardi@outlook.com>
2020-12-19 14:38:23 -0300

    SellerDaoJDBC and Factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 				
 		Department dpt = new Department(1, "Books");
 		System.out.println(dpt);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller slr = new Seller(21, "Bob", "bob@gamil.com", new Date(), 3000.0, dpt);
 		System.out.println(slr);
