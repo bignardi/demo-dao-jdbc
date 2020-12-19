b3fae59e994ed95a41977337aeb740288d6d6ff0
Vinicius Bignardi <vinicius.bignardi@outlook.com>
2020-12-18 23:02:23 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+				
+		Department dpt = new Department(1, "Books");
+		System.out.println(dpt);
+	}
+
+}
